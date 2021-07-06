bf0ad4af4f1aa7c4c6fd7667cba440defbfaac83
billingsmoore <61596772+billingsmoore@users.noreply.github.com>
2021-07-06 14:51:59 -0400

    Add files via upload

---
+++ Number Game/number.py
@@ -0,0 +1,42 @@
+#import random number generator
+from random import randint
+
+#set screen size
+WIDTH = 400
+HEIGHT = 400
+
+#set up lists
+dots = []
+lines = []
+
+next_dot = 0
+
+#set up actors
+for dot in range(0, 10):
+    actor = Actor("dot")
+    actor.pos = randint(20, (WIDTH - 20)), randint(20, (HEIGHT - 20))
+    dots.append(actor)
+
+#define draw function
+def draw():
+    screen.fill("black")
+    number = 1
+    #draw the dots
+    for dot in dots:
+        screen.draw.text(str(number), (dot.pos[0], dot.pos[1] + 12))
+        dot.draw()
+        number = number + 1
+    #draw the lines
+    for line in lines:
+        screen.draw.line(line[0], line[1], (100, 0, 0))
+
+def on_mouse_down(pos):
+    global next_dot
+    global lines
+    if dots[next_dot].collidepoint(pos):
+        if next_dot:
+            lines.append((dots[next_dot - 1].pos, dots[next_dot].pos))
+        next_dot = next_dot + 1
+    else:
+        lines = []
+        next_dot = 0
